0e453bfb8fd26b98990d637ea7c0875c554e2f0b
nurulakbaral <nurulakbaral@gmail.com>
2021-11-12 09:35:23 +0700

    updates: init setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Rak Sandi</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
-
-export default MyApp
