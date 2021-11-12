49445fa00c93aa17ed21966e41c5b8b823985197
nurulakbaral <nurulakbaral@gmail.com>
2021-11-12 10:08:23 +0700

    chore: change jargon

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div className='mt-12'>
-      <h1 className='text-center text-2xl font-cairo'>Hello World!</h1>
+      <h1 className='text-center text-2xl font-cairo'>Rak Sandi: Rapikan Kata Sandi Anda Seperti Sepatu!</h1>
       <div className='flex flex-col items-center'>
         <input type='text' />
         <input type='text' />
