615095df1998697fec4792056630c11b33748cba
nurulakbaral <nurulakbaral@gmail.com>
2021-11-13 09:45:06 +0700

    chore: fix setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,10 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div className='mt-20'>
-      <h1 className='text-center text-2xl font-cairo'>Rak Sandi - Rapikan kata sandi Anda seperti sepatu di rak </h1>
-      <h1 className='mt-6 font-poppins h-full leading-normal font-extrabold text-5xl text-center text-transparent bg-clip-text  bg-gradient-to-r from-green-400 to-blue-500'>
+      <h1 className='text-center text-2xl font-cairo leading-normal text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-400'>
+        Rak Sandi - Rapikan kata sandi Anda seperti sepatu di rak{' '}
+      </h1>
+      <h1 className='mt-6 font-poppins h-full leading-normal font-extrabold text-5xl text-center text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>
         Coming Soon!
       </h1>
     </div>
