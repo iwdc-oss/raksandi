899a1ba9818b220b566581d2bc03580af942d8b4
nurulakbaral <nurulakbaral@gmail.com>
2021-11-16 20:32:53 +0700

    updates: dialog-add-password done

--- src/components/button/ButtonBase.tsx
+++ src/components/button/ButtonBase.tsx
@@ -3,14 +3,19 @@ import React from 'react'
 export const ButtonBase = ({
   children,
   className,
+  textColor = 'text-gray-600',
+  onClick,
   ...props
 }: {
   children: React.ReactNode
   className?: string
+  textColor?: string
+  onClick?(): void
 }): JSX.Element => {
   return (
     <button
-      className={`${className} hover:bg-gray-100 rounded-md border-2 border-gray-400 text-gray-600 w-full h-full text-xl font-semibold`}
+      onClick={onClick}
+      className={`${className} hover:bg-gray-100 tracking-wider px-2 rounded-md border-2 border-gray-400 ${textColor} w-full h-full text-base sm:text-sm font-semibold`}
       {...props}
     >
       {children}
--- src/components/input/InputBase.tsx
+++ src/components/input/InputBase.tsx
@@ -4,6 +4,8 @@ export const InputBase = ({
   name,
   type,
   className,
+  onChange,
+  onBlur,
   ...props
 }: {
   name: string
@@ -11,9 +13,13 @@ export const InputBase = ({
   className?: string
   value?: string
   disabled?: boolean
+  onChange?(e: React.ChangeEvent<HTMLInputElement>): void
+  onBlur?(e: React.FocusEvent<HTMLInputElement>): void
 }): JSX.Element => {
   return (
     <input
+      onChange={onChange}
+      onBlur={onBlur}
       className={`rounded-md border-[1px] border-gray-300 w-full h-full ${className}`}
       name={name}
       type={type}
