2e844f898c6c8d70f36469a03db6527fa2e59317
nurulakbaral <nurulakbaral@gmail.com>
2021-11-20 17:51:34 +0700

    chore: ui improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { signIn, useSession, getProviders, ClientSafeProvider } from 'next-auth/
 import { ButtonBase } from '~/src/components/button/ButtonBase'
 import { FcGoogle } from 'react-icons/fc'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 type Providers = {
   google: ClientSafeProvider
@@ -9,6 +10,7 @@ type Providers = {
 
 export default function Home({ providers }: { providers: Providers }) {
   const [session, loading] = useSession()
+  const router = useRouter()
   const { google } = providers
   if (loading) {
     return <p className='text-center mt-36'>Loading...</p>
@@ -18,7 +20,13 @@ export default function Home({ providers }: { providers: Providers }) {
       <h1 className='text-4xl font-bold text-center font-cairo mb-4'>Rak Sandi</h1>
       <h1 className='text-center text-2xl font-cairo'>Rapikan kata sandi Anda seperti sepatu di rak </h1>
       <div className='text-center'>
-        <Image alt='Rak Sandi' src='/images/illustration/ill_dashboard.svg' width={400} height={400} quality={100} />
+        <Image
+          alt='Rak Sandi'
+          src={`/images/illustration/${session ? 'ill_dashboard.svg' : 'ill_home.svg'}`}
+          width={400}
+          height={400}
+          quality={100}
+        />
       </div>
       {!session && (
         <div className='h-14 w-3/4 mx-auto mb-12'>
@@ -32,6 +40,11 @@ export default function Home({ providers }: { providers: Providers }) {
           >
             Log in with Google
           </ButtonBase>
+        </div>
+      )}
+      {session && (
+        <div className='h-14 w-3/4 mx-auto'>
+          <ButtonBase onClick={() => router.push('/dashboard')}>{`Let's create your passwords`}</ButtonBase>
         </div>
       )}
     </div>
